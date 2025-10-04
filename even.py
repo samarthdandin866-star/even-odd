2afe2c6388276623d53339d3f7de55870cf6d760
Samarth <samarthdandin866@gmail.com>
2025-10-04 10:40:53 +0530

    save

--- even.py
+++ even.py
@@ -8,3 +8,19 @@ if num % 2 == 0:
     print(f"{num} is Even.")
 else:
     print(f"{num} is Odd.")
+
+# Program to check if a number is prime
+
+# Take input from user
+num = int(input("Enter a number: "))
+
+# Prime numbers are greater than 1
+if num > 1:
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            print(f"{num} is NOT a Prime Number.")
+            break
+    else:
+        print(f"{num} is a Prime Number.")
+else:
+    print(f"{num} is NOT a Prime Number.")
